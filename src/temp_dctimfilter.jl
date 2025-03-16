c3a98c39ea5e05b833143a22520d6c6a6627b878
gschivre <geoffrey.schivre@gmail.com>
2025-03-16 17:45:50 +0100

    WIP

--- src/temp_dctimfilter.jl
+++ src/temp_dctimfilter.jl
@@ -38,14 +38,15 @@
             dctdst_2first!(buffer, Z, img, kernel, Ibegin, ind, Iend, border)
         end
 
-        # Propagate forwards
-        for i = range(first(ind) + k, stop=ind[end-1])
+        # Propagate forwards up to the R + 1 filtered coefficients for which we need to access out of bounds values
+        for i in range(first(ind) + 2; length = R - 1)
             @inbounds for Ibegin in Rbegin
-                tmp = accumfilter(img[Ibegin, i, Iend], one(T))
-                for j = 1:k
-                    tmp += kernel.a[j] * safe_for_prod(out[Ibegin, i-j, Iend], tmp)
-                end
-                out[Ibegin, i, Iend] = tmp
+            end
+        end
+
+        # Propagate forwards
+        for i in (first(ind) + R + 1):(ind[end])
+            @inbounds for Ibegin in Rbegin
             end
         end
     end
@@ -62,25 +63,25 @@ function dctdst_2first!(buffer, Z, img, kernel, Ibegin, ind, Iend, border::Pad)
     border.style == :symmetric && return _dctdst_2first_symmetric!(buffer, Z, img, kernel, Ibegin, ind, Iend)
     border.style == :reflect && return _dctdst_2first_reflect!(buffer, Z, img, kernel, Ibegin, ind, Iend)
 end
-function _dctdst_2first_uniqueval!(buffer, Z, img, kernel, Ibegin, ind, Iend, borderval)
+function _dctdst_2first_uniqueval!(buffer, Z, img, kernel::DCTFilter{T}, Ibegin, ind, Iend, borderval) where T
     C = kernel.basis
     h = kernel.coefficients
     R, K = size(C)
     for k in 1:K
         Z[k, :] .= accumfilter(borderval, sum(view(C, 2:R, k)))
         Z[k, 1] -= accumfilter(borderval, C[2, k]) 
-        Z[k, 1] += accumfilter(out[Ibegin, ind[firstindex(ind)], Iend], C[2, k])
+        Z[k, 1] += accumfilter(img[Ibegin, ind[firstindex(ind)], Iend], C[2, k])
     end
     n = 0
     for i in firstindex(ind):(firstindex(ind) + R - 1)
         n += 1
         for k in 1:K
-            Z[k, 1] += accumfilter(out[Ibegin, ind[i], Iend], C[n, K])
-            Z[k, 2] += accumfilter(out[Ibegin, ind[i + 1], Iend], C[n, K])
+            Z[k, 1] += accumfilter(img[Ibegin, ind[i], Iend], C[n, K])
+            Z[k, 2] += accumfilter(img[Ibegin, ind[i + 1], Iend], C[n, K])
         end
     end
-    tmp1 = zero(eltype(h))
-    tmp2 = zero(eltype(h))
+    tmp1 = zero(T)
+    tmp2 = zero(T)
     for k in 1:K
         Z[k, 1] = accumfilter(Z[k, 1], h[k])
         tmp1 += Z[k, 1]
